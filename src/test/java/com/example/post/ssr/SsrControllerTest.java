deb964b2129d5e471546684e82374ed9039dffa3
Toshiaki Maki <makingx@gmail.com>
2024-04-08 23:25:20 +0900

    Use HtmlUnit

--- src/test/java/com/example/post/ssr/SsrControllerTest.java
+++ src/test/java/com/example/post/ssr/SsrControllerTest.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import com.example.post.Post;
 import com.example.post.PostService;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlScript;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +17,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SsrController.class)
 @Import(ReactRenderer.class)
@@ -25,6 +28,9 @@ class SsrControllerTest {
 	@Autowired
 	MockMvc mvc;
 
+	@Autowired
+	WebClient webClient;
+
 	@MockBean
 	PostService postService;
 
@@ -32,31 +38,32 @@ class SsrControllerTest {
 	void index() throws Exception {
 		given(this.postService.getPosts()).willReturn(List.of(new Post(2, "SSR", "Server Side Rendering!", 1000),
 				new Post(1, "Hello", "Hello World!", 1000)));
-		MvcResult result = this.mvc.perform(get("/")).andExpect(status().isOk()).andReturn();
-		String html = result.getResponse().getContentAsString();
-		assertThat(html).contains(
-				"""
-						<div id="root"><div><h2>Posts</h2><h3><a href="/posts/2">SSR</a></h3><h3><a href="/posts/1">Hello</a></h3></div></div>
-						""");
-		assertThat(html).contains(
-				"""
-						<script id="__INIT_DATA__" type="application/json">{"preLoadedPosts":[{"id":2,"title":"SSR","body":"Server Side Rendering!","userId":1000},{"id":1,"title":"Hello","body":"Hello World!","userId":1000}]}</script>
-						""");
+		HtmlPage page = this.webClient.getPage("/");
+		HtmlDivision root = page.getHtmlElementById("root");
+		assertThat(root).isNotNull();
+		DomNodeList<DomNode> titles = root.querySelectorAll("h3");
+		assertThat(titles).hasSize(2);
+		assertThat(titles.get(0).getTextContent()).isEqualTo("SSR");
+		assertThat(titles.get(1).getTextContent()).isEqualTo("Hello");
 	}
 
 	@Test
 	void post() throws Exception {
 		given(this.postService.getPost(1)).willReturn(new Post(1, "Hello", "Hello World!", 1000));
-		MvcResult result = this.mvc.perform(get("/posts/1")).andExpect(status().isOk()).andReturn();
-		String html = result.getResponse().getContentAsString();
-		assertThat(html).contains(
-				"""
-						<div id="root"><h3><a href="/posts/1">Hello</a></h3><p>Hello World!</p><hr/><a href="/">« Go to Posts</a></div>
-						""");
-		assertThat(html).contains(
-				"""
-						<script id="__INIT_DATA__" type="application/json">{"preLoadedPost":{"id":1,"title":"Hello","body":"Hello World!","userId":1000}}</script>
-						""");
+		HtmlPage page = this.webClient.getPage("/posts/1");
+		HtmlDivision root = page.getHtmlElementById("root");
+		assertThat(root).isNotNull();
+		DomNode title = root.querySelector("h3");
+		assertThat(title).isNotNull();
+		assertThat(title.getTextContent()).isEqualTo("Hello");
+		DomNode body = root.querySelector("p");
+		assertThat(body).isNotNull();
+		assertThat(body.getTextContent()).isEqualTo("Hello World!");
+		HtmlScript initData = page.getHtmlElementById("__INIT_DATA__");
+		assertThat(initData).isNotNull();
+		assertThat(initData.getTextContent()).isEqualTo("""
+				{"preLoadedPost":{"id":1,"title":"Hello","body":"Hello World!","userId":1000}}
+				""".trim());
 	}
 
 }
