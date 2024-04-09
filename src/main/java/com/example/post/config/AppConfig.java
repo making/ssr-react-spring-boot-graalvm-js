bdd26cd10cf2a6e1e3f0290cab4a02fb5e7579cc
Toshiaki Maki <makingx@gmail.com>
2024-04-09 18:44:25 +0900

    Add access logeer

---
+++ src/main/java/com/example/post/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.example.post.config;
+
+import am.ik.accesslogger.AccessLogger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class AppConfig {
+
+	@Bean
+	public AccessLogger accessLogger() {
+		return new AccessLogger(httpExchange -> {
+			String uri = httpExchange.getRequest().getUri().getPath();
+			return uri != null && !(uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator"));
+		});
+	}
+
+}
